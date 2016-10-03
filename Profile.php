d6678b2ac9fdc903a84fd48e7122989a00a52879
codeapps <codeapps@aol.com>
2016-10-02 21:55:59 -0500

    Complete and Incomplete Count and Totals

--- Profile.php
+++ Profile.php
@@ -6,7 +6,7 @@ use Colors\Color;
 
 $color = new Color();
 
-$name = "Name.txt";
+$name = "Name.md";
 $data = fopen($name, "r");
 
 if (file_exists($name)) {
@@ -19,8 +19,11 @@ if (file_exists($name)) {
   fclose($data);
 }
 
-$profile = "Profile.txt";
+$profile = "Profile.md";
 $data = fopen($profile, "r");
+
+$complete = 0;
+$incomplete = 0;
 
 if (file_exists($profile)) {
   while (($lines = fgets($data)) !== false) {
@@ -40,10 +43,12 @@ if (file_exists($profile)) {
       }
       elseif (preg_match('/\[\]/', $line)) {
         $line = substr($line, 3);
+        $incomplete += 1;
       	echo '[' . $color('Incomplete')->red() . '] ' . $color($line)->white();
       }
       elseif (preg_match('/\[X\]/i', $line)) {
         $line = substr($line, 3);
+        $complete += 1;
       	echo '[' . $color('Complete')->green() . ']' . $color($line)->white();
       }
       else {
@@ -53,6 +58,16 @@ if (file_exists($profile)) {
     echo PHP_EOL;
   }
   fclose($data);
+
+  $total = $complete + $incomplete;
+
+  $complete_porcentage = (100 / $total) * $complete;
+  $incomplete_porcentage = (100/ $total) * $incomplete;
+
+  echo $color($complete_porcentage . '% Complete: ' . $complete)->white()->bold()->highlight('green');
+  echo PHP_EOL;
+  echo $color($incomplete_porcentage . '% Incomplete: ' . $incomplete)->white()->bold()->highlight('red');
+  echo PHP_EOL;
 }
 
 ?>
